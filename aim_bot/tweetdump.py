71238c77f5c83175f88008be64420144dc29c73c
gradiuscypher <gradiuscypher@gmail.com>
2017-01-10 11:01:54 -0800

    using tweepy instead to see how streaming works there

--- aim_bot/tweetdump.py
+++ aim_bot/tweetdump.py
@@ -1,34 +1,58 @@
 #!/usr/bin/env python
 
-import twitter
+import tweepy
 import configparser
-import json
+import pprint
 import argparse
+import requests
 
 
-def stream_twitter(screen_name):
-    user_id = t_client.users.lookup(screen_name=screen_name)[0]['id']
+class StreamTweets(tweepy.StreamListener):
+    def __init__(self, webhook):
+        super().__init__()
+        self.webhook = webhook
 
-    stream = twitter.stream.TwitterStream(auth=auth, domain='stream.twitter.com')
+    def on_status(self, status):
+        # TODO: can be replaced with chatbot alert library later when it is complete
+        if self.webhook is not None:
+            message_text = ""
 
-    for message in stream.statuses.filter(follow=user_id):
-        print(json.dumps(message))
+            message_text += "**@" + status.user.screen_name + "**\n"
+            message_text += status.text + "\n"
+
+            requests.post(self.webhook, data={"content": message_text, "username": "Twitter Monitor"})
+        else:
+            pprint.pprint(repr(status))
+
+
+def stream_twitter(username_list, webhook):
+    userid_list = []
+
+    for username in username_list:
+        userid_list.append(str(api.get_user(username).id))
+
+    tweet_listener = StreamTweets(webhook)
+    tweet_stream = tweepy.Stream(auth=api.auth, listener=tweet_listener)
+    tweet_stream.filter(userid_list)
 
 
 def dump_twitter(screen_name, count=100, include_rts=0):
-    tweets = t_client.statuses.user_timeline(include_rts=include_rts, screen_name=screen_name, count=count)
-    for tweet in tweets:
-        print(json.dumps(tweet))
+    # TODO: Re-implement with Tweepy and validate dump limit
+    # tweets = t_client.statuses.user_timeline(include_rts=include_rts, screen_name=screen_name, count=count)
+    # for tweet in tweets:
+    #     print(json.dumps(tweet))
+    pass
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="tcpdump style tool for twitter. can stream tweets or dump profiles")
+    parser.add_argument('config', help='Config file name')
+    parser.add_argument('--userlist', help="List of target user names", nargs='+', required=True)
     parser.add_argument('--dump', help='Dump a full twitter profile.', action='store_true')
     parser.add_argument('--stream', help='Dump a full twitter profile.', action='store_true')
     parser.add_argument('--retweets', help='Include retweets in a dump', action='store_true')
-    parser.add_argument('-count', help='Number of tweets to pull down when dumping')
-    parser.add_argument('config', help='Config file name')
-    parser.add_argument('user', help="Target twitter user name")
+    parser.add_argument('--count', help='Number of tweets to pull down when dumping')
+    parser.add_argument('--webhook', help='Webhook to dump messages to')
     opts = parser.parse_args()
 
     config = configparser.RawConfigParser()
@@ -37,20 +61,22 @@ if __name__ == "__main__":
     consumer_secret = config.get("Twitter", "consumer_secret")
     access_token = config.get("Twitter", "access_token")
     access_secret = config.get("Twitter", "access_secret")
-    auth = twitter.OAuth(consumer_key=consumer_key, consumer_secret=consumer_secret, token=access_token,
-                         token_secret=access_secret)
-    t_client = twitter.Twitter(auth=auth)
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    api = tweepy.API(auth)
 
     if opts.stream:
-        stream_twitter(opts.user)
+        stream_twitter(opts.userlist, webhook=opts.webhook)
 
-    if opts.dump:
-        if opts.retweets:
-            include_retweets = 1
-        else:
-            include_retweets = 0
-
-        if opts.count is not None:
-            dump_twitter(opts.user, count=opts.count, include_rts=include_retweets)
-        else:
-            dump_twitter(opts.user, include_rts=include_retweets)
+    # TODO: Re-implement with Tweepy and validate dump limit
+    # if opts.dump:
+    #     if opts.retweets:
+    #         include_retweets = 1
+    #     else:
+    #         include_retweets = 0
+    #
+    #     if opts.count is not None:
+    #         dump_twitter(opts.user, count=opts.count, include_rts=include_retweets)
+    #     else:
+    #         dump_twitter(opts.user, include_rts=include_retweets)
