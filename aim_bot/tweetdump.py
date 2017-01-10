2a1de335bd1698afda7bfb7c72cf6ad294965208
gradiuscypher <gradiuscypher@gmail.com>
2017-01-10 14:33:43 -0800

    tons of formatting changes

--- aim_bot/tweetdump.py
+++ aim_bot/tweetdump.py
@@ -5,33 +5,57 @@ import configparser
 import pprint
 import argparse
 import requests
+import traceback
 
 
 class StreamTweets(tweepy.StreamListener):
-    def __init__(self, webhook):
+    def __init__(self, webhook, debug):
         super().__init__()
         self.webhook = webhook
+        self.debug = debug
 
     def on_status(self, status):
+        if self.debug:
+            pprint.pprint(repr(status))
+
         # TODO: can be replaced with chatbot alert library later when it is complete
         if self.webhook is not None:
-            message_text = ""
+            try:
+                embed_blob = {}
+                fields = []
+                embed_blob['title'] = "@{}".format(status.user.screen_name)
+                embed_blob['thumbnail'] = {"url": status.user.profile_image_url_https}
+                fields.append({"name": "Status", "value": status.text})
 
-            message_text += "**@" + status.user.screen_name + "**\n"
-            message_text += status.text + "\n"
+                if 'media' in status.entities:
+                    embed_blob['image'] = {"url": status.entities['media'][0]["media_url_https"]}
 
-            requests.post(self.webhook, data={"content": message_text, "username": "Twitter Monitor"})
+                if 'urls' in status.entities and len(status.entities['urls']):
+                    urls = []
+                    for u in status.entities['urls']:
+                        urls.append(u['expanded_url'])
+                    fields.append({"name": "Expanded URLs", "value": str(urls)})
+
+                embed_blob["fields"] = fields
+
+                result = requests.post(self.webhook, json={"username": "Twitter Monitor", "embeds": [embed_blob]})
+
+                if result.status_code != 204:
+                    print("[{}] - {}".format(result.status_code, result.text))
+
+            except:
+                print(traceback.format_exc())
         else:
             pprint.pprint(repr(status))
 
 
-def stream_twitter(username_list, webhook):
+def stream_twitter(username_list, webhook, debug):
     userid_list = []
 
     for username in username_list:
         userid_list.append(str(api.get_user(username).id))
 
-    tweet_listener = StreamTweets(webhook)
+    tweet_listener = StreamTweets(webhook, debug)
     tweet_stream = tweepy.Stream(auth=api.auth, listener=tweet_listener)
     tweet_stream.filter(userid_list)
 
@@ -53,6 +77,7 @@ if __name__ == "__main__":
     parser.add_argument('--retweets', help='Include retweets in a dump', action='store_true')
     parser.add_argument('--count', help='Number of tweets to pull down when dumping')
     parser.add_argument('--webhook', help='Webhook to dump messages to')
+    parser.add_argument('--debug', help="Print lots of extra stuff for debugging", action='store_true')
     opts = parser.parse_args()
 
     config = configparser.RawConfigParser()
@@ -67,7 +92,7 @@ if __name__ == "__main__":
     api = tweepy.API(auth)
 
     if opts.stream:
-        stream_twitter(opts.userlist, webhook=opts.webhook)
+        stream_twitter(opts.userlist, webhook=opts.webhook, debug=opts.debug)
 
     # TODO: Re-implement with Tweepy and validate dump limit
     # if opts.dump:
